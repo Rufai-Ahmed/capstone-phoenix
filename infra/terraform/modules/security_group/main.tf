7fa635a66f7de9e5c4e5d441fdc81ff15734ca3e
Rufai-Ahmed <abbeyrufai234@gmail.com>
2026-06-25 15:15:03 +0100

    fix(sg): drop '>' from rule descriptions (AWS rejects it)

--- infra/terraform/modules/security_group/main.tf
+++ infra/terraform/modules/security_group/main.tf
@@ -23,7 +23,7 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
 
 resource "aws_vpc_security_group_ingress_rule" "http" {
   security_group_id = aws_security_group.node.id
-  description       = "HTTP -> ingress-nginx"
+  description       = "HTTP to ingress-nginx"
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   to_port           = 80
@@ -32,7 +32,7 @@ resource "aws_vpc_security_group_ingress_rule" "http" {
 
 resource "aws_vpc_security_group_ingress_rule" "https" {
   security_group_id = aws_security_group.node.id
-  description       = "HTTPS -> ingress-nginx"
+  description       = "HTTPS to ingress-nginx"
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   to_port           = 443
